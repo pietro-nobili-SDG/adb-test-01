81f0be833ea3681ecfddbb7e14c61ccf8d0d0968
pietro.nobili <pietro.nobili@sdggroup.com>
2022-05-06 10:43:04 +0200

    Add samples of data, script and notebook

---
+++ data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def load_sample_data():
+    """"""
+    return pd.DataFrame({"a": [1, 3, 5, 6]})
---
+++ main.py
@@ -0,0 +1,5 @@
+from data import load_sample_data
+
+if __name__ == "__main__":
+    df = load_sample_data()
+    print(df)
